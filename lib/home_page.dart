824c0bc591bccfbf60f509668b9fe17c5b2702ab
Eduardo <echavezv@utea.edu.pe>
2025-01-23 07:40:06 -0500

    ElevateButton

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -61,7 +61,62 @@ class HomePage extends StatelessWidget {
               // overlayColor: Color(Colors.amber) ,
               // thumbColor: Colors.red,
             ),
-            Slider(value: 70, min: 20, max: 200, onChanged: (double value) {}),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  "175",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF003049),
+                  ),
+                ),
+                SizedBox(
+                  width: 2.0,
+                ),
+                Text(
+                  "cm",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w300,
+                    color: Color(0xFF003049),
+                  ),
+                ),
+              ],
+            ),
+            Slider(
+              value: 70,
+              min: 20,
+              max: 200,
+              onChanged: (double value) {},
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50.0,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                label: Text(
+                  "Calcular",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                icon: Icon(Icons.play_arrow_rounded),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF003049),
+                ),
+              ),
+            ),
           ],
         ),
       ),
